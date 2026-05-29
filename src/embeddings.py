fe22417ed5024d673cfb6d9affeccbb8240175a9
Giran Oh <giranoh12@gmail.com>
2026-05-29 13:48:34 +0900

    feat: embeddings.py 구현 - dataset.py 구현 안해서 테스트 안돌아감

--- src/embeddings.py
+++ src/embeddings.py
@@ -26,8 +26,14 @@ class InputEmbedding(nn.Module):
         super().__init__()
         self.emb_dim = emb_dim
         self.context_length = context_length
+
         # TODO: token_embedding, position_embedding, dropout을 정의하세요.
-        raise NotImplementedError("InputEmbedding.__init__을 구현하세요.")
+        # nn.Embedding: 임베딩 층을 랜덤으로 초기화해서 객체를 만들어줌.
+        self.token_embedding = nn.Embedding(vocab_size, emb_dim)
+        self.position_embedding = nn.Embedding(context_length, emb_dim)
+        self.dropout = nn.Dropout(drop_rate)
+        
+
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
@@ -39,4 +45,16 @@ class InputEmbedding(nn.Module):
         Returns:
             (batch_size, seq_len, emb_dim)
         """
-        raise NotImplementedError("InputEmbedding.forward를 구현하세요.")
+        # shape[0] = 행 크기 반환 (batch_size), shape[1] = 열 크기 반환 (seq_len)
+        seq_len = x.shape[1]
+        # 임베딩 벡터에 넣을 벡터 크기
+        positions = torch.arange(seq_len)
+
+        # 행렬 모양이 달라보이지만 파이토치가 자동으로 pos_emb에 tok_emb 배치를 복사해서 더해줌으로써 차원 맞춤 (브로드캐스팅)
+        tok_emb = self.token_embedding(x)            # [batch_size, seq_len, emb_dim]
+        pos_emb = self.position_embedding(positions) # [seq_len, emb_dim]
+
+        # 토큰 임베딩값과 위치 임베딩값 더한거 드롭아웃 적용하고 반환함
+        return self.dropout(tok_emb + pos_emb)
+
+
